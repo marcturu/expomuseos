6de34e75cbe82f265f6856599fbdd7187986ade7
Marc Turu <marcturu80@gmail.com>
2026-02-26 10:29:26 +0100

    Delete routes directory

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\MuseumApiController;
-
-Route::get('/museums/{page?}', [MuseumApiController::class, 'museums']);
-Route::get('/museum/{id}', [MuseumApiController::class, 'museum']);
-Route::get('/topic/{id}/{page?}', [MuseumApiController::class, 'topic']);
